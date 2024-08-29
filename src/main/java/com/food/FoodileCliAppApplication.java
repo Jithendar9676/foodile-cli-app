afa8d869e5aa044f62f8c17e2576b40379be7ca4
jithendar9676 <jithendarreddy9676@gmail.com>
2024-08-29 23:48:27 +0530

    chore:initial

---
+++ src/main/java/com/food/FoodileCliAppApplication.java
@@ -0,0 +1,13 @@
+package com.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodileCliAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodileCliAppApplication.class, args);
+	}
+
+}
